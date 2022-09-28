8324105b62127b73e314cf46a9434b9622e6de46
Benjamin Q <benking3502@yahoo.com>
2022-09-28 10:09:53 -0400

    Complete add song form + song card js
    - Complete add song form
    - Add main song card js
    - Mostly format song cards including basic (unfinished) heard
    checkbox but not a 'remove song' button yet

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@
 
 let myLibrary = [];
 
-function Book(title, author, album, length, haveHeard) {
+function Song(title, author, album, length, haveHeard) {
   this.title = title;
   this.author = author;
   this.album = album;
@@ -19,33 +19,88 @@ function Book(title, author, album, length, haveHeard) {
   }
 }
 
-// const theHobbit = new Book('The Hobbit', 'JRR Tolkien', 295, false);
+// const theHobbit = new Song('The Hobbit', 'JRR Tolkien', 295, false);
 // console.log(theHobbit.info());
 
 //Example strInput: 'The Hobbit, JRR Tolkien, 295, false'
-function addBookToLibrary(strInput) {
-  let strArray = strInput.split(', ');
-  let book = new Book(strArray[0], strArray[1], strArray[2], strArray[3]);
-  myLibrary.push(book);
+function addSongToLibrary(song) {
+  // let strArray = strInput.split(', ');
+  // let song = new Song(strArray[0], strArray[1], strArray[2], strArray[3]);
+  myLibrary.push(song);
+  displaySongs();
 }
 
-let bookContainer = document.querySelector('.book-container');
-function displayBooks(myLibrary) {
-  for (let book of myLibrary) {
-    let bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-    bookCard.textContent = book.info();
-    bookContainer.appendChild(bookCard);
+function displaySongs() {
+  let songContainer = document.querySelector('.song-container');
+  songContainer.innerHTML = ''; // Removes all song cards to redraw them
+  for (let song of myLibrary) {
+    let songCard = document.createElement('div');
+    songCard.classList.add('song-card');
+
+    let songNameElem = document.createElement('div');
+    songNameElem.classList.add('song-name');
+    songNameElem.textContent = `Song: ${song.title}`;
+    songCard.appendChild(songNameElem);
+
+    let songAuthorElem = document.createElement('div');
+    songAuthorElem.classList.add('song-author');
+    songAuthorElem.textContent = `Author: ${song.author}`;
+    songCard.appendChild(songAuthorElem);
+
+    let songAlbumElem = document.createElement('div');
+    songAlbumElem.classList.add('song-album');
+    songAlbumElem.textContent = `Album: ${song.album}`;
+    songCard.appendChild(songAlbumElem);
+
+    let songLengthElem = document.createElement('div');
+    songLengthElem.classList.add('song-length');
+    songLengthElem.textContent = `Length: ${song.length}`;
+    songCard.appendChild(songLengthElem);
+
+    let songHeardElem = document.createElement('div');
+    songHeardElem.classList.add('song-heard-switch');
+    songHeardElem.innerHTML = `
+      Heard: 
+      <label class="switch">
+        <input type="checkbox">
+        <span class="slider round"></span>
+      </label>
+    `;
+    songCard.appendChild(songHeardElem);
+
+    songContainer.appendChild(songCard);
   }
 }
 
-addBookToLibrary('The Hobbit, JRR Tolkien, 295, false')
-addBookToLibrary('The Hobbit2, JRR Tolkien2, 295, false')
-addBookToLibrary('The Hobbit3, JRR Tolkien3, 295, false')
-displayBooks(myLibrary)
+// addSongToLibrary('The Hobbit, JRR Tolkien, 295, false');
+// addSongToLibrary('The Hobbit2, JRR Tolkien2, 295, false');
+// addSongToLibrary('The Hobbit3, JRR Tolkien3, 295, false');
+// displaySongs();
 
-let addSongButton = document.querySelector('.add-song-button');
-addSongButton.addEventListener('click', () => {
+function hideForm() {
   let formElement = document.querySelector('.form-container');
   formElement.style.display = (formElement.style.display === 'initial' ? 'none' : 'initial');
-});
+}
+
+let addSongButton = document.querySelector('.add-song-button');
+addSongButton.addEventListener('click', hideForm);
+
+function saveInput() {
+  let elements = document.querySelectorAll('.add-song-form input');
+  console.log(elements);
+  console.log(elements.length);
+  let inputsArray = [];
+  if (elements[0].value !== '') {
+    for (let element of elements) {
+      if (element.type === 'checkbox') {
+        inputsArray.push(element.checked);
+      } else if (element.id === 'album' && element.value === '') {
+        inputsArray.push('N/A');
+      } else {
+        inputsArray.push(element.value);
+      }
+    }
+    let song = new Song(...inputsArray);
+    addSongToLibrary(song);
+  }
+}
